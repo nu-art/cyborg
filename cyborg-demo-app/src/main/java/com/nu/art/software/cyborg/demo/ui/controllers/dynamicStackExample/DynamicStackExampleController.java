c12d48326d0a67d2c9cbc03eb9dbb0141e0220b5
TacB0sS <adam.zehavi@nu-art-software.com>
2015-07-24 03:52:22 +0300

    Working Example of the stack transition animations

--- cyborg-demo-app/src/main/java/com/nu/art/software/cyborg/demo/ui/controllers/dynamicStackExample/DynamicStackExampleController.java
+++ cyborg-demo-app/src/main/java/com/nu/art/software/cyborg/demo/ui/controllers/dynamicStackExample/DynamicStackExampleController.java
@@ -7,17 +7,26 @@
 
 package com.nu.art.software.cyborg.demo.ui.controllers.dynamicStackExample;
 
+import android.graphics.Typeface;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.nu.art.software.core.exceptions.runtime.MUST_NeverHappenedException;
 import com.nu.art.software.cyborg.annotations.Restorable;
 import com.nu.art.software.cyborg.annotations.ViewIdentifier;
 import com.nu.art.software.cyborg.common.consts.ViewListener;
+import com.nu.art.software.cyborg.core.CyborgAdapter;
 import com.nu.art.software.cyborg.core.CyborgController;
+import com.nu.art.software.cyborg.core.ItemRenderer;
+import com.nu.art.software.cyborg.core.dataModels.ListDataModel;
 import com.nu.art.software.cyborg.core.interfaces.StackManagerEventListener;
 import com.nu.art.software.cyborg.demo.R;
+import com.nu.art.software.cyborg.ui.animations.AnimationTransition;
+import com.nu.art.software.cyborg.ui.animations.PredefinedAnimations;
 import com.nu.art.software.reflection.annotations.ReflectiveInitialization;
 
 @ReflectiveInitialization
@@ -33,6 +42,12 @@ public class DynamicStackExampleController
 	@ViewIdentifier(viewId = R.id.AddToLeftStack, listeners = ViewListener.OnCheckChanged)
 	private CheckBox addToLeftStack;
 
+	@ViewIdentifier(viewId = R.id.AddToRightStack, listeners = ViewListener.OnCheckChanged)
+	private CheckBox addToRightStack;
+
+	@ViewIdentifier(viewId = R.id.AnimationSelector, listeners = ViewListener.OnItemSelected)
+	private Spinner animationSelector;
+
 	@Restorable
 	private String toSave;
 
@@ -44,6 +59,10 @@ public class DynamicStackExampleController
 
 	private StackManagerEventListener stack;
 
+	private int counter;
+
+	private PredefinedAnimations selectedAnimation = PredefinedAnimations.values()[0];
+
 	public DynamicStackExampleController() {
 		super(R.layout.v1_controller__stack_example);
 	}
@@ -51,6 +70,21 @@ public class DynamicStackExampleController
 	@Override
 	public void onCreate() {
 		stack = getController(StackManagerEventListener.class, RightPane);
+		CyborgAdapter<PredefinedAnimations> adapter = new CyborgAdapter<PredefinedAnimations>(activityBridge, AnimationItemRenderer.class) {
+
+			@Override
+			@SuppressWarnings("unchecked")
+			protected <RendererType extends ItemRenderer<? extends PredefinedAnimations>> RendererType instantiateItemRendererType(Class<RendererType> renderersType) {
+				if (renderersType == AnimationItemRenderer.class)
+					return (RendererType) new AnimationItemRenderer();
+				throw new MUST_NeverHappenedException("");
+			}
+		};
+		ListDataModel<PredefinedAnimations> dataModel = new ListDataModel<PredefinedAnimations>(PredefinedAnimations.class);
+		dataModel.addItems(PredefinedAnimations.values());
+		adapter.setDataModel(dataModel);
+		animationSelector.setAdapter(adapter.getArrayAdapter());
+		addToRightStack.setChecked(true);
 		updateStackLabel();
 	}
 
@@ -64,17 +98,36 @@ public class DynamicStackExampleController
 
 	@Override
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		stack = getController(StackManagerEventListener.class, !isChecked ? RightPane : LeftPane);
+		CheckBox toUncheck;
+		String stackTag;
+		switch (buttonView.getId()) {
+			case R.id.AddToLeftStack:
+				toUncheck = addToRightStack;
+				stackTag = isChecked ? LeftPane : RightPane;
+				break;
+			case R.id.AddToRightStack:
+				toUncheck = addToLeftStack;
+				stackTag = isChecked ? RightPane : LeftPane;
+
+				break;
+			default:
+				throw new MUST_NeverHappenedException("");
+		}
+		toUncheck.setChecked(!isChecked);
+		stack = getController(StackManagerEventListener.class, stackTag);
 	}
 
-	int counter;
+	@Override
+	public void onItemSelected(AdapterView<?> parentView, View selectedView, int position, long id) {
+		selectedAnimation = PredefinedAnimations.values()[position];
+	}
 
 	@Override
 	public void onClick(View v) {
 		counter++;
 		switch (v.getId()) {
 			case R.id.AddA:
-				stack.push("TagA-" + counter, DynamicAFragmentController.class, true);
+				stack.push("TagA-" + counter, DynamicAFragmentController.class, new AnimationTransition(getActivity(), selectedAnimation), true);
 				break;
 			case R.id.AddB:
 				stack.push("TagB-" + counter, R.layout.v1_controller__dynamic_b, true);
@@ -91,5 +144,22 @@ public class DynamicStackExampleController
 		boolean b = stack.popLast();
 		updateStackLabel();
 		return b;
+	}
+
+	public class AnimationItemRenderer
+			extends ItemRenderer<PredefinedAnimations> {
+
+		@ViewIdentifier(viewId = R.id.ExampleLabel)
+		TextView exampleLabel;
+
+		private AnimationItemRenderer() {
+			super(R.layout.list_node__recycler_example_float);
+		}
+
+		@Override
+		protected void renderItem(PredefinedAnimations item) {
+			exampleLabel.setText(item.name());
+			exampleLabel.setTypeface(null, item == selectedAnimation ? Typeface.BOLD : Typeface.NORMAL);
+		}
 	}
 }
---
+++ cyborg-demo-app/src/main/java/com/nu/art/software/cyborg/demo/ui/controllers/transitionAnimation/TransitionAnimationBeginExampleController.java
@@ -0,0 +1,93 @@
+package com.nu.art.software.cyborg.demo.ui.controllers.transitionAnimation;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.nu.art.software.cyborg.annotations.ViewIdentifier;
+import com.nu.art.software.cyborg.common.consts.ViewListener;
+import com.nu.art.software.cyborg.core.CyborgController;
+import com.nu.art.software.cyborg.core.interfaces.StackManagerEventListener;
+import com.nu.art.software.cyborg.demo.R;
+import com.nu.art.software.cyborg.ui.animations.LeadingViewAnimationBuilder;
+import com.nu.art.software.cyborg.ui.animations.LeadingViewAnimationBuilder.ViewGenerator;
+import com.nu.art.software.cyborg.ui.animations.LeadingViewAnimationBuilder.ViewResolver;
+
+/**
+ * Created by TacB0sS on 14-Jul 2015.
+ */
+public class TransitionAnimationBeginExampleController
+		extends CyborgController {
+
+	@ViewIdentifier(viewId = R.id.imageView, listeners = ViewListener.OnClick)
+	private ImageView image;
+
+	@ViewIdentifier(viewId = R.id.textView4, listeners = ViewListener.OnClick)
+	private TextView text;
+
+	@ViewIdentifier(viewId = R.id.ratingBar, listeners = ViewListener.OnClick)
+	private RatingBar ratingBar;
+
+	private StackManagerEventListener stack;
+
+	private static final String AnimationTransitionStack = "AnimationTransition";
+
+	public TransitionAnimationBeginExampleController() {
+		super(R.layout.v1_controller__transition_animation_begin_example);
+	}
+
+	@Override
+	public void onCreate() {
+		stack = getController(StackManagerEventListener.class, AnimationTransitionStack);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void onClick(View v) {
+		LeadingViewAnimationBuilder leadingViewAnimation;
+		switch (v.getId()) {
+			case R.id.imageView:
+				leadingViewAnimation = new LeadingViewAnimationBuilder<ImageView>(getActivity());
+				leadingViewAnimation
+						.setViewResolver(new ViewResolver<ImageView, TransitionAnimationBeginExampleController, TransitionAnimationEndExampleController>() {
+							@Override
+							public ImageView resolveOriginView(TransitionAnimationBeginExampleController transitionAnimationBeginExampleController) {
+								return transitionAnimationBeginExampleController.getImage();
+							}
+
+							@Override
+							public ImageView resolveTargetView(TransitionAnimationEndExampleController transitionAnimationEndExampleController) {
+								return transitionAnimationEndExampleController.getImage();
+							}
+						});
+				leadingViewAnimation.setViewGenerator(new ViewGenerator<ImageView>() {
+					@Override
+					public ImageView generate(ImageView origin, ImageView target) {
+						ImageView view = new ImageView(origin.getContext());
+						view.setImageDrawable(origin.getDrawable());
+						return view;
+					}
+				});
+				stack.push("AnimationEnd", TransitionAnimationEndExampleController.class, true);
+				break;
+			case R.id.textView:
+				break;
+			case R.id.ratingBar:
+				break;
+		}
+	}
+
+	public RatingBar getRatingBar() {
+		return ratingBar;
+	}
+
+	public TextView getText() {
+		return text;
+	}
+
+	public ImageView getImage() {
+		return image;
+	}
+
+}
