4b295f15fb05d4f221d7535651795b8510b3fc65
TacB0sS <adam.zehavi@nu-art-software.com>
2017-10-01 17:18:09 +0300

    manual build with Minor version to promote

--- test--build-modules.sh
+++ test--build-modules.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash ./dev-tools/scripts/build/build-modules.sh "ALL" "uploadArchives" master false Version
+bash ./dev-tools/scripts/build/build-modules.sh "ALL" "uploadArchives" master false Minor
