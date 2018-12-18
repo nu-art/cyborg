515f10275c4646e7fc90615a6adc968dcc399754
TacB0sS <adam.vdk@nu-art-software.com>
2018-12-18 22:14:01 +0200

    added viewpager example

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash dev-tools/scripts/dev/build-and-install.sh --package-name=com.nu.art.cyborg.demo --project=app-demo-cyborg --app-name="Cyborg Demo" "$@"
+bash dev-tools/scripts/dev/build-and-install.sh --package-name=com.nu.art.cyborg.demo --project=app-demo-cyborg --app-name="Cyborg Demo" --build=debug "$@"
