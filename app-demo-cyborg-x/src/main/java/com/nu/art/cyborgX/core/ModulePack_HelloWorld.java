2ff5c3bda98f1d0e6e58bd50036009f5a613be6f
TacB0sS <adam.vdk@nu-art-software.com>
2020-08-20 14:09:48 +0300

    removed stupid copyright

--- app-demo-cyborg-x/src/main/java/com/nu/art/cyborgX/core/ModulePack_HelloWorld.java
+++ app-demo-cyborg-x/src/main/java/com/nu/art/cyborgX/core/ModulePack_HelloWorld.java
@@ -1,12 +1,4 @@
-/*
- * Copyright (c) 2017 to Adam van der Kruk (Zehavi) AKA TacB0sS - Nu-Art Software
- *
- * This software code is not an 'Open Source'!
- * In order to use this code you MUST have a proper license.
- * In order to obtain a licence please contact me directly.
- *
- * Email: adam.vdk@nu-art-software.com
- */
+
 
 package com.nu.art.cyborgX.core;
 
